de19c9b45b59cceb2118ae6e5e45ef561055cdbb
syed ismail <63254031+SyedIsmail14@users.noreply.github.com>
2026-01-04 19:27:36 +0530

    Update character-charades.js

--- js/character-charades.js
+++ js/character-charades.js
@@ -4,36 +4,7 @@
    ===================================================== */
 
 const CHARADES = [
-  "Spider Man",
-  "Batman",
-  "Superman",
-  "Iron Man",
-  "Wonder Woman",
-  "Harry Potter",
-  "Mickey Mouse",
-  "Donald Duck",
-  "Tom and Jerry",
-  "Dora Explorer",
-  "Ninja Turtle",
-  "Pikachu",
-  "SpongeBob",
-  "Scooby Doo",
-  "Minion",
-  "Elsa Frozen",
-  "Aladdin",
-  "Simba Lion King",
-  "Buzz Lightyear",
-  "Woody Toy Story",
-  "Shrek",
-  "Kung Fu Panda",
-  "Captain America",
-  "Hulk",
-  "Thor",
-  "Joker",
-  "Mario",
-  "Luigi",
-  "Princess Peach",
-  "Popeye"
+"Luke Skywalker","Darth Vader","Princess Leia","Han Solo","Yoda","Obi Wan Kenobi","Anakin Skywalker","Rey","Kylo Ren","Finn","Poe Dameron","Chewbacca","R2 D2","C 3PO","BB 8","Lando Calrissian","Padme Amidala","Mace Windu","Qui Gon Jinn","Count Dooku","General Grievous","Boba Fett","Jango Fett","Ahsoka Tano","Emperor Palpatine","Tony Stark","Steve Rogers","Thor Odinson","Bruce Banner","Natasha Romanoff","Clint Barton","Peter Parker","Stephen Strange","Wanda Maximoff","Vision","Sam Wilson","Bucky Barnes","T Challa","Shuri","Okoye","Nick Fury","Phil Coulson","Pepper Potts","James Rhodes","Scott Lang","Hope Van Dyne","Carol Danvers","Peter Quill","Gamora","Drax","Rocket Raccoon","Groot","Mantis","Nebula","Thanos","Loki","Hela","Ultron","Red Skull","Winter Soldier","Falcon","War Machine","Hawkeye","Black Widow","Scarlet Witch","Quicksilver","Agent Carter","Sharon Carter","Maria Hill","Heimdall","Valkyrie","Korg","Wong","Ancient One","Mordo","Christine Palmer","Jane Foster","Darcy Lewis","Erik Selvig","Happy Hogan","Aunt May","Uncle Ben","Mary Jane","Gwen Stacy","Green Goblin","Doctor Octopus","Venom","Sandman","Electro","Vulture","Mysterio","Thaddeus Ross","Betty Ross","Abomination","Red Hulk","She Hulk","Moon Knight","Daredevil","Punisher","Jessica Jones","Luke Cage","Iron Fist","Elektra Natchios","Foggy Nelson","Karen Page","Wilson Fisk","Bullseye","Bruce Wayne","Clark Kent","Diana Prince","Barry Allen","Arthur Curry","Hal Jordan","Oliver Queen","Kara Zor El","Victor Stone","Billy Batson","John Constantine","Alfred Pennyworth","Commissioner Gordon","Robin","Nightwing","Red Hood","Batgirl","Catwoman","Harley Quinn","Joker","Riddler","Penguin","Two Face","Scarecrow","Bane","Ra's al Ghul","Poison Ivy","Mr Freeze","Clayface","Killer Croc","Lex Luthor","Brainiac","Doomsday","General Zod","Darkseid","Steppenwolf","Ares","Cheetah","Black Manta","Ocean Master","Sinestro","Atrocitus","Reverse Flash","Zoom","Captain Cold","Heat Wave","Deathstroke","Amanda Waller","Rick Flag","Deadshot","El Diablo","Killer Croc","Enchantress","Katana","Boomerang","King Shark","Polka Dot Man","Peacemaker","Bloodsport","Ratcatcher","John Wick","Ethan Hunt","James Bond","Jason Bourne","John McClane","Martin Riggs","Roger Murtaff","Axel Foley","Maverick","Goose","Iceman","Viper","Rooster","Hangman","Phoenix","Bob","Dominic Toretto","Brian O'Conner","Letty Ortiz","Mia Toretto","Roman Pearce","Tej Parker","Ramsey","Luke Hobbs","Deckard Shaw","Owen Shaw","Cipher","Jakob Toretto","Han Seoul Oh","Gisele Yashar","Leon","Vince","Jesse","Suki","Sean Boswell","Twinkie"
 ];
 
 /* SHUFFLE */
